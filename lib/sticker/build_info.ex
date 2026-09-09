8895135a327f1410ad99301c321a1ca028ec67f8
bikai <34392010@qq.com>
2026-09-09 16:19:06 +0800

    Read build metadata at runtime

--- lib/sticker/build_info.ex
+++ lib/sticker/build_info.ex
@@ -1,8 +1,6 @@
 defmodule Sticker.BuildInfo do
   @moduledoc "Public identifiers baked into the release, never credentials."
-  @revision System.get_env("BUILD_REVISION", "local")
-  @built_at System.get_env("BUILD_TIME", "unknown")
 
-  def revision, do: @revision
-  def built_at, do: @built_at
+  def revision, do: System.get_env("BUILD_REVISION", "local")
+  def built_at, do: System.get_env("BUILD_TIME", "unknown")
 end
